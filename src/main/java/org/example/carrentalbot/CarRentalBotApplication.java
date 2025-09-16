799316632648df84ba92b0d0ca0fdb371ea8b643
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-09-16 21:30:56 +0200

    initial commit

---
+++ src/main/java/org/example/carrentalbot/CarRentalBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.carrentalbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalBotApplication.class, args);
+    }
+
+}

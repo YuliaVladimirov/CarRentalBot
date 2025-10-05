c71e225c13923024aedb694230e6097cd817ce9e
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-10-05 13:21:25 +0200

    feat: add new callback handler for main menu display

---
+++ src/main/java/org/example/carrentalbot/handler/GoToMainMenuHandler.java
@@ -0,0 +1,47 @@
+package org.example.carrentalbot.handler;
+
+import org.example.carrentalbot.dto.CallbackQueryDto;
+import org.example.carrentalbot.dto.InlineKeyboardMarkupDto;
+import org.example.carrentalbot.dto.SendMessageDto;
+import org.example.carrentalbot.service.NavigationService;
+import org.example.carrentalbot.util.KeyboardFactory;
+import org.example.carrentalbot.util.TelegramClient;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GoToMainMenuHandler implements CallbackHandler {
+
+    public static final String KEY = "GO_TO_MAIN_MENU";
+
+    private final NavigationService navigationService;
+    private final KeyboardFactory keyboardFactory;
+    private final TelegramClient telegramClient;
+
+    public GoToMainMenuHandler(NavigationService navigationService,
+                               KeyboardFactory keyboardFactory,
+                               TelegramClient telegramClient) {
+        this.navigationService = navigationService;
+        this.keyboardFactory = keyboardFactory;
+        this.telegramClient = telegramClient;
+    }
+
+    @Override
+    public String getKey() {
+        return KEY;
+    }
+
+    @Override
+    public void handle(Long chatId, CallbackQueryDto callbackQuery) {
+
+        InlineKeyboardMarkupDto replyMarkup = keyboardFactory.buildMainMenuKeyboard();
+
+        navigationService.push(chatId, KEY);
+        telegramClient.sendMessage(SendMessageDto.builder()
+                .chatId(chatId.toString())
+                .text("Main Menu")
+                .parseMode("HTML")
+                .replyMarkup(replyMarkup)
+                .build());
+
+    }
+}

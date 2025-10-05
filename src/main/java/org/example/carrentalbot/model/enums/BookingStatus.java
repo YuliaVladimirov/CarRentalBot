ff5e509a003465619c8173db508887056d2c8b8a
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-10-05 11:52:55 +0200

    refactor: include string value in enum definition

--- src/main/java/org/example/carrentalbot/model/enums/BookingStatus.java
+++ src/main/java/org/example/carrentalbot/model/enums/BookingStatus.java
@@ -1,7 +1,16 @@
 package org.example.carrentalbot.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum BookingStatus {
-    PENDING,
-    CONFIRMED,
-    CANCELLED
+    PENDING ("Pending"),
+    CONFIRMED ("Confirmed"),
+    CANCELLED ("Cancelled");
+
+    private final String value;
+
+    BookingStatus(String value) {
+        this.value = value;
+    }
 }
--- src/main/java/org/example/carrentalbot/model/enums/CarCategory.java
+++ src/main/java/org/example/carrentalbot/model/enums/CarCategory.java
@@ -1,9 +1,18 @@
 package org.example.carrentalbot.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum CarCategory {
-    SEDAN,
-    SUV,
-    HATCHBACK,
-    CONVERTIBLE,
-    VAN
+    SEDAN("Sedan"),
+    SUV("SUV"),
+    HATCHBACK("Hatchback"),
+    CONVERTIBLE("Convertible"),
+    VAN("Van");
+
+    private final String value;
+
+    CarCategory(String value) {
+        this.value = value;
+    }
 }

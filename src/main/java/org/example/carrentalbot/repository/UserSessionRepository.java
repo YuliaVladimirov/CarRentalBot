63fb6d97060f150ed084a10e50d4927ff91a627b
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-10-08 09:17:09 +0200

    refactor: delete unnecessary classes

--- src/main/java/org/example/carrentalbot/repository/UserSessionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.carrentalbot.repository;
-
-import org.example.carrentalbot.model.UserSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSessionRepository extends JpaRepository<UserSession, Long> {
-}

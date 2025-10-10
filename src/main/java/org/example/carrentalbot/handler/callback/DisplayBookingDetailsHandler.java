756ae2fd34a091d6e35c7a110d134e5df090fa86
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-10-10 07:42:53 +0200

    refactor: extract code into helper methods

--- src/main/java/org/example/carrentalbot/handler/callback/DisplayBookingDetailsHandler.java
+++ src/main/java/org/example/carrentalbot/handler/callback/DisplayBookingDetailsHandler.java
@@ -5,6 +5,7 @@ import org.example.carrentalbot.dto.InlineKeyboardMarkupDto;
 import org.example.carrentalbot.dto.SendPhotoDto;
 import org.example.carrentalbot.exception.DataNotFoundException;
 import org.example.carrentalbot.model.Car;
+import org.example.carrentalbot.service.BookingService;
 import org.example.carrentalbot.service.CarService;
 import org.example.carrentalbot.service.NavigationService;
 import org.example.carrentalbot.service.SessionService;
@@ -16,7 +17,6 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 @Component
@@ -26,17 +26,21 @@ public static final String KEY = "DISPLAY_BOOKING_DETAILS";
 
     private final NavigationService navigationService;
     private final SessionService sessionService;
-    private final TelegramClient telegramClient;
     private final CarService carService;
+    private final BookingService bookingService;
     private final KeyboardFactory keyboardFactory;
+    private final TelegramClient telegramClient;
 
-    public DisplayBookingDetailsHandler(NavigationService navigationService, SessionService sessionService,
-                                        TelegramClient telegramClient, CarService carService, KeyboardFactory keyboardFactory) {
+    public DisplayBookingDetailsHandler(NavigationService navigationService,
+                                        SessionService sessionService,
+                                        CarService carService, BookingService bookingService, KeyboardFactory keyboardFactory,
+                                        TelegramClient telegramClient) {
         this.navigationService = navigationService;
         this.sessionService = sessionService;
-        this.telegramClient = telegramClient;
         this.carService = carService;
+        this.bookingService = bookingService;
         this.keyboardFactory = keyboardFactory;
+        this.telegramClient = telegramClient;
     }
 
     @Override
@@ -49,20 +53,23 @@ public static final String KEY = "DISPLAY_BOOKING_DETAILS";
 
         UUID carId = sessionService.get(chatId, "carId", UUID.class).orElseThrow(() -> new DataNotFoundException(chatId, "Car id not found"));
 
-        Car car = carService.getCarInfo(carId).orElseThrow(() -> new DataNotFoundException(chatId, String.format("Car with id: %s, was not found.", carId)));
-
         LocalDate startDate = sessionService.get(chatId, "startDate", LocalDate.class).orElseThrow(() -> new DataNotFoundException(chatId, "Start date not found"));
+
         LocalDate endDate = sessionService.get(chatId, "endDate", LocalDate.class).orElseThrow(() -> new DataNotFoundException(chatId, "End date not found"));
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-        BigDecimal dailyRate = car.getDailyRate().setScale(0, RoundingMode.HALF_UP);
-
-        long totalDays = ChronoUnit.DAYS.between(startDate, endDate);
-        BigDecimal totalCost = car.getDailyRate().multiply(BigDecimal.valueOf(totalDays));
 
         String phone = sessionService.get(chatId, "phone", String.class).orElseThrow(() -> new DataNotFoundException(chatId, "Phone not found"));
 
         String email = sessionService.get(chatId, "email", String.class).orElseThrow(() -> new DataNotFoundException(chatId, "Email not found"));
+
+        Car car = carService.getCar(chatId, carId);
+
+        BigDecimal dailyRate = car.getDailyRate().setScale(0, RoundingMode.HALF_UP);
+
+        long totalDays = bookingService.calculateTotalDays(startDate, endDate);
+        BigDecimal totalCost = bookingService.calculateTotalCost(dailyRate, totalDays);
+        sessionService.put(chatId, "totalCost", totalCost);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
         String text = String.format("""
                 <b>Your booking details:</b>

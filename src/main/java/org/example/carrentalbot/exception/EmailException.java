a91332e00227f8da70294afa7436690c84bc7aa4
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-12-05 19:56:24 +0100

    refactor: remove unnecessary classes

--- src/main/java/org/example/carrentalbot/exception/EmailException.java
+++
@@ -1,7 +0,0 @@
-package org.example.carrentalbot.exception;
-
-public class EmailException extends RuntimeException {
-    public EmailException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}

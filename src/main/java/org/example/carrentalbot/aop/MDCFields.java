22e25173fa6613c41e5c928149356a326ea66407
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-12-08 01:16:27 +0100

    refactor: change MDC fields format to json format

--- src/main/java/org/example/carrentalbot/aop/MDCFields.java
+++ src/main/java/org/example/carrentalbot/aop/MDCFields.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 @Getter
 public enum MDCFields {
 
-    TRACE_ID ("traceId"),
+    TRACE_ID ("trace_id"),
 
-    UPDATE_ID ("updateId"),
-    UPDATE_TYPE ("updateType"),
-    CHAT_ID ("chatId"),
-    TELEGRAM_USER_ID ("telegramUserId"),
+    UPDATE_ID ("update_id"),
+    UPDATE_TYPE ("update_type"),
+    CHAT_ID ("chat_id"),
+    TELEGRAM_USER_ID ("telegram_user_id"),
 
     CLASS ("class"),
     METHOD ("method");

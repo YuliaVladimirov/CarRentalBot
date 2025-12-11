f5821bddaf6d1d982c3126cb243bf7497e49ccca
Yulia Vladimirov <julia.vladimirov@gmail.com>
2025-12-11 23:19:38 +0100

    docs: style improvements in JavaDoc

--- src/main/java/org/example/carrentalbot/controller/WebhookController.java
+++ src/main/java/org/example/carrentalbot/controller/WebhookController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
  *
  * <p>All valid updates are delegated to {@link GlobalHandler} for processing.</p>
  */
-
 @Slf4j
 @RestController
 @RequestMapping("/webhook")
